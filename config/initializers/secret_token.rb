b7a93029efd4fade02dff670ab4b52c9b5acf64f
Daniel Esplin <daniel@custombit.com>
2012-08-13 17:06:25 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Danielesplin::Application.config.secret_token = 'c1499e22237e67d170f1fa8d181b28881f3dbb3f0170d816267ea7fda820e052fcb8a30a02254a47d73380b34c02772318486ca7e8dd3187b0405e60098bb50f'
