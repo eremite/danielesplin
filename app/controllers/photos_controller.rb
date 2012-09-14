74f4230c24cd6d7b719db416ac40e1bc1d96616c
Daniel Esplin <daniel@custombit.com>
2012-09-13 19:38:41 -0600

    Add photo create.
    No idea why attr_accessible is required.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,30 @@
 class PhotosController < ApplicationController
 
+  def index
+    @photos = Photo.all
+  end
+
   def new
     @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(safe_params)
+    if @photo.save
+      redirect_to photos_url
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def safe_params
+    safe_attributes = [
+      :description,
+    ]
+    params.require(:photo).permit(*safe_attributes)
   end
 
 end
--- test/functional/photos_controller_test.rb
+++ test/functional/photos_controller_test.rb
@@ -4,12 +4,35 @@ class PhotosControllerTest < ActionController::TestCase
 
   test 'index' do
     get :index
-    assert_template 'index'
+    assert_template :index
   end
 
   test 'new' do
     get :new
-    assert_template 'new'
+    assert_template :new
+  end
+
+  test 'create invalid' do
+    Photo.any_instance.stubs(valid?: false)
+    post :create, photo: valid_params
+    assert_template :new
+  end
+
+  test 'create valid' do
+    Photo.any_instance.stubs(valid?: true)
+    post :create, photo: valid_params
+    assert_redirected_to photos_url
+  end
+
+
+  private
+
+  def valid_params
+    {
+      photo: {
+        description: 'fred'
+      }
+    }
   end
 
 end
