6042da2a947904a513147fa5259a1fd6bffd7083
Daniel Esplin <daniel@custombit.com>
2012-10-12 17:27:20 -0600

    Extract date when uploading photos.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -8,4 +8,24 @@ class Photo < ActiveRecord::Base
 
   scope :oldest_first, order(arel_table[:at].asc)
 
+  before_save :extract_at
+
+  private
+
+  def extract_at
+    if image?
+      image.manipulate! do |img|
+        date = img['EXIF:DateTimeOriginal'].to_s.squish
+        if date.present?
+          begin
+            self.at = Time.zone.parse(date.sub(':', '-').sub(':', '-'))
+          rescue ArgumentError, TypeError
+            logger.error "#{date} is an invalid date."
+          end
+          img
+        end
+      end
+    end
+  end
+
 end
