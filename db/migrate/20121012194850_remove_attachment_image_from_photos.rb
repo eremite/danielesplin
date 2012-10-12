e85c3a6bfd8e6f319c7ee3eebbfd09540bfd711d
Daniel Esplin <daniel@custombit.com>
2012-10-12 15:37:03 -0600

    Comment out now unnecessary migration.

--- db/migrate/20121012194850_remove_attachment_image_from_photos.rb
+++ db/migrate/20121012194850_remove_attachment_image_from_photos.rb
@@ -1,15 +1,16 @@
 class RemoveAttachmentImageFromPhotos < ActiveRecord::Migration
+  # We no longer have paperclip installed...
   def up
-    remove_column :photos, :image_file_name
-    remove_column :photos, :image_content_type
-    remove_column :photos, :image_file_size
-    remove_column :photos, :image_updated_at
+    # remove_column :photos, :image_file_name
+    # remove_column :photos, :image_content_type
+    # remove_column :photos, :image_file_size
+    # remove_column :photos, :image_updated_at
   end
 
   def down
-    add_column :photos, :image_updated_at, :datetime
-    add_column :photos, :image_file_size, :integer
-    add_column :photos, :image_content_type, :string
-    add_column :photos, :image_file_name, :string
+    # add_column :photos, :image_updated_at, :datetime
+    # add_column :photos, :image_file_size, :integer
+    # add_column :photos, :image_content_type, :string
+    # add_column :photos, :image_file_name, :string
   end
 end
