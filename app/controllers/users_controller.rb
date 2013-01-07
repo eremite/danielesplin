58896688c8d7b2d42ec51ffc12f1217eedf8f376
Daniel Esplin <daniel@custombit.com>
2013-01-07 09:03:09 -0700

    Add user create and delete.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,25 @@ class UsersController < ApplicationController
 
   load_and_authorize_resource
 
+  def create
+    if @user.save
+      redirect_to users_url, notice: 'User created.'
+    else
+      render :new
+    end
+  end
+
   def update
     if @user.update_attributes(params[:user])
       redirect_to edit_user_url(@user), notice: 'Changes saved.'
     else
       render :edit
     end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_url, notice: 'User deleted.'
   end
 
 end
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -2,32 +2,56 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
 
-  def setup
-    @user = users(:base)
-    login_as(@user)
-  end
-
   test 'index' do
     login_as(users(:admin))
     get :index
     assert_template :index
   end
 
+  test 'new' do
+    login_as(users(:admin))
+    get :new
+    assert_template :new
+  end
+
+  test 'create invalid' do
+    login_as(users(:admin))
+    User.any_instance.stubs(save: false)
+    post :create, user: {}
+    assert_template :new
+  end
+
+  test 'create valid' do
+    login_as(users(:admin))
+    User.any_instance.stubs(save: true)
+    post :create, user: {}
+    assert_redirected_to users_url
+  end
+
   test 'edit' do
-    get :edit, id: @user.id
+    login_as(users(:base))
+    get :edit, id: users(:base).id
     assert_template :edit
   end
 
   test 'update invalid' do
+    login_as(users(:base))
     User.any_instance.stubs(valid?: false)
-    put :update, id: @user.id, user: {}
+    put :update, id: users(:base).id, user: {}
     assert_template :edit
   end
 
   test 'update valid' do
+    login_as(users(:base))
     User.any_instance.stubs(valid?: true)
-    put :update, id: @user.id, user: {}
-    assert_redirected_to edit_user_url(@user)
+    put :update, id: users(:base).id, user: {}
+    assert_redirected_to edit_user_url(users(:base))
+  end
+
+  test 'destroy' do
+    login_as(users(:admin))
+    delete :destroy, id: users(:base).id
+    assert_redirected_to users_url
   end
 
 end
