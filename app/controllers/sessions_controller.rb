06130f23f6b243012e98060ece473833eddcbbf1
Daniel Esplin <daniel@custombit.com>
2013-03-02 19:29:26 -0700

    Remove flash message after logging out.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: 'You have been logged out.'
+    redirect_to root_path
   end
 
 end
