a26b28d27a6de450d2aba0df1e9ecc0cb22d01f7
Daniel Esplin <daniel@custombit.com>
2013-05-16 18:04:48 -0600

    Allow creating journal entries for babies.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,9 +1,13 @@
 class Entry < ActiveRecord::Base
 
-  attr_accessible :at, :body, :public
+  attr_accessor :baby_body
+
+  attr_accessible :at, :body, :public, :baby_body
 
   belongs_to :user
   has_many :comments
+
+  after_create :create_baby_entry, if: lambda { |e| e.baby_body.present? }
 
   validates :body, presence: true
 
@@ -22,6 +26,24 @@ class Entry < ActiveRecord::Base
     conditions = t[:at].in(at.beginning_of_day..at.end_of_day).and(t[:entry_id].eq(nil))
     conditions = conditions.or(t[:entry_id].eq(id))
     Photo.where(conditions)
+  end
+
+  def baby_body
+    return @baby_body if defined?(@baby_body)
+    baby = User.where(email: 'baby@danielesplin.org').first
+    time = Time.zone.now
+    @baby_body = baby.entries.where(:at => time.beginning_of_day..time.end_of_day).first
+  end
+
+
+  private
+
+  def create_baby_entry
+    baby = User.where(email: 'baby@danielesplin.org').first
+    baby.entries.create({
+      at: at,
+      body: baby_body,
+    }) if baby
   end
 
 end
