ba356a8712babe1960386f5c568f42a1b5764988
Daniel Esplin <daniel@custombit.com>
2013-11-08 07:55:44 -0700

    Fix syntax error in deploy script.
    Pro tip: it's hard to program with a baby.

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,22 +1,20 @@
 resource = new_resource
 rails_environment = resource.environment['RAILS_ENV']
 
-  Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_environment}...")
-  execute 'rake assets:precompile' do
-    cwd resource.current_path
-    command 'bundle exec rake assets:precompile'
-    environment 'RAILS_ENV' => rails_environment
-  end
+Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_environment}...")
+execute 'rake assets:precompile' do
+  cwd resource.current_path
+  command 'bundle exec rake assets:precompile'
+  environment 'RAILS_ENV' => rails_environment
+end
 
-  template "#{resource.current_path}/shared/config/application.yml" do
-    source "#{resource.current_path}/deploy/templates/application.yml.erb"
-    mode '0660'
-    group resource.group
-    owner resource.user
-    variables(:environment_variables => resource.params[:deploy_data]['environment_variables'])
-    only_if do
-      File.exists?("#{resource.current_path}") && File.exists?("#{resource.current_path}/shared/config/")
-    end
+template "#{resource.current_path}/shared/config/application.yml" do
+  source "#{resource.current_path}/deploy/templates/application.yml.erb"
+  mode '0660'
+  group resource.group
+  owner resource.user
+  variables(:environment_variables => resource.params[:deploy_data]['environment_variables'])
+  only_if do
+    File.exists?("#{resource.current_path}") && File.exists?("#{resource.current_path}/shared/config/")
   end
-
 end
