d2f726bd824938de0d4d3f393fba8e6db0b00a66
Daniel Esplin <daniel@custombit.com>
2013-11-08 06:33:11 -0700

    Add deploy code for setting environment variables.

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,7 +1,24 @@
-rails_env = new_resource.environment['RAILS_ENV']
-Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
-execute 'rake assets:precompile' do
-  cwd release_path
-  command 'bundle exec rake assets:precompile'
-  environment 'RAILS_ENV' => rails_env
+include_recipe 'deploy'
+
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+
+  Chef::Log.info("Precompiling assets for RAILS_ENV=#{deploy[:rails_env]}...")
+  execute 'rake assets:precompile' do
+    cwd deploy[:deploy_to]
+    command 'bundle exec rake assets:precompile'
+    environment 'RAILS_ENV' => deploy[:rails_env]
+  end
+
+  template "#{deploy[:deploy_to]}/shared/config/application.yml" do
+    source "#{deploy[:deploy_to]}/deploy/templates/application.yml.erb"
+    mode '0660'
+    group deploy[:group]
+    owner deploy[:user]
+    variables(environment_variables: deploy[:environment_variables])
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/shared/config/")
+    end
+  end
+
 end
