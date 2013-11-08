dfb10e8991eff4c9a69322d86ef32455fac449be
Daniel Esplin <daniel@custombit.com>
2013-11-08 07:48:58 -0700

    Use resource instead of deploy for deploying.

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,25 +1,21 @@
-Chef::Log.debug("node: #{new_resource.inspect}")
-Chef::Log.debug("node: #{new_resource.environment.inspect}")
-Chef::Log.debug("node: #{node.inspect}")
+resource = new_resource
+rails_environment = resource.environment['RAILS_ENV']
 
-node[:deploy].each do |application, deploy|
-  deploy = node[:deploy][application]
-
-  Chef::Log.info("Precompiling assets for RAILS_ENV=#{deploy[:rails_env]}...")
+  Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_environment}...")
   execute 'rake assets:precompile' do
-    cwd deploy[:deploy_to]
+    cwd resource.current_path
     command 'bundle exec rake assets:precompile'
-    environment 'RAILS_ENV' => deploy[:rails_env]
+    environment 'RAILS_ENV' => rails_environment
   end
 
-  template "#{deploy[:deploy_to]}/shared/config/application.yml" do
-    source "#{deploy[:deploy_to]}/deploy/templates/application.yml.erb"
+  template "#{resource.current_path}/shared/config/application.yml" do
+    source "#{resource.current_path}/deploy/templates/application.yml.erb"
     mode '0660'
-    group deploy[:group]
-    owner deploy[:user]
-    variables(:environment_variables => deploy[:environment_variables])
+    group resource.group
+    owner resource.user
+    variables(:environment_variables => resource.params[:deploy_data]['environment_variables'])
     only_if do
-      File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/shared/config/")
+      File.exists?("#{resource.current_path}") && File.exists?("#{resource.current_path}/shared/config/")
     end
   end
 
