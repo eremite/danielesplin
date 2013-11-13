ff5f3f8caf8c8b5a8c78360473004af8d2b335fc
Daniel Esplin <daniel@custombit.com>
2013-11-12 20:07:22 -0700

    Attempt to fix precompiling assets.

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,5 +1,4 @@
 resource = new_resource
-rails_environment = resource.environment['RAILS_ENV']
 
 file "#{resource.shared_path}/config/application.yml" do
   owner resource.user
@@ -10,11 +9,21 @@ file "#{resource.shared_path}/config/application.yml" do
   only_if { ::File.exists?(resource.current_path)}
 end
 
-Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_environment}...")
+directory "#{resource.shared_path}/assets" do
+  owner resource.user
+  group resource.group
+  mode '0660'
+  action :create
+  recursive true
+end
+
+link "#{resource.current_path}/public/assets" do
+  to "#{resource.shared_path}/assets"
+end
+
 execute 'rake assets:precompile' do
   cwd resource.current_path
   command 'bundle exec rake assets:precompile'
-  environment 'RAILS_ENV' => rails_environment
+  environment 'RAILS_ENV' => resource.environment['RAILS_ENV']
   only_if { ::File.exists?(resource.current_path)}
 end
-
