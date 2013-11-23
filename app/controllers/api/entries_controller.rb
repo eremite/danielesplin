f864f5aa24729f8ef9aa04163e5602b8108986b5
Daniel Esplin <daniel@custombit.com>
2013-11-23 13:46:36 -0700

    Use strong_params in api/entries_controller.

--- app/controllers/api/entries_controller.rb
+++ app/controllers/api/entries_controller.rb
@@ -3,12 +3,19 @@ class Api::EntriesController < ApiController
   respond_to :json
 
   def create
-    @entry = @user.entries.new(params[:entry])
+    @entry = @user.entries.new(safe_params)
     if @entry.save
       respond_with(@entry, status: :created)
     else
       render text: @entry.errors.full_messages.to_sentence, status: :not_acceptable
     end
+  end
+
+
+  private
+
+  def safe_params
+    params.require(:entry).permit(:at, :body, :public, :baby_body)
   end
 
 end
