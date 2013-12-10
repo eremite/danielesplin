5f8b898070fad2b39e120cc41159a9fc8b064737
Daniel Esplin <daniel@custombit.com>
2013-12-10 10:49:31 -0700

    Create uploads directory on deploy.

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -6,12 +6,20 @@ file "#{resource.shared_path}/config/application.yml" do
   mode '0660'
   content resource.params[:deploy_data]['environment_variables'].map { |k, v| "#{k.upcase}: #{v}" }.join("\n")
   action :create
-  only_if { ::File.exists?(resource.shared_path)}
+  only_if { ::File.exists?(resource.shared_path) }
+end
+
+directory "#{resource.shared_path}/uploads" do
+  owner resource.user
+  group resource.group
+  mode '0660'
+  action :create
+  only_if { ::File.exists?(resource.shared_path) }
 end
 
 execute 'rake assets:precompile' do
   cwd release_path
   command 'bundle exec rake assets:precompile'
   environment 'RAILS_ENV' => resource.environment['RAILS_ENV']
-  only_if { ::File.exists?(release_path)}
+  only_if { ::File.exists?(release_path) }
 end
