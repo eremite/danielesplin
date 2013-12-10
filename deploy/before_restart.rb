00e6cdadf0270fb128dc5ab07eec9a138315db9f
Daniel Esplin <daniel@custombit.com>
2013-12-10 08:00:47 -0700

    Add bin to before_restart script.

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,7 +1,7 @@
 resource = new_resource
 execute 'delayed_job restart' do
   cwd release_path
-  command 'delayed_job restart'
+  command 'bin/delayed_job restart'
   environment 'RAILS_ENV' => resource.environment['RAILS_ENV']
   only_if { ::File.exists?(release_path)}
 end
