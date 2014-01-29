51bfbe61912343f95a82acac401567fcdb9d2c9b
Daniel Esplin <daniel@custombit.com>
2014-01-29 13:57:48 -0700

    Switch to S3 instead of Google Storage.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    provider: 'Google',
-    google_storage_access_key_id: Figaro.env.fog_key.to_s,
-    google_storage_secret_access_key: Figaro.env.fog_secret.to_s,
+    provider: 'AWS',
+    aws_access_key_id: Figaro.env.fog_key.to_s,
+    aws_secret_access_key: Figaro.env.fog_secret.to_s,
   }
   config.fog_directory = Figaro.env.fog_bucket.to_s
 end
