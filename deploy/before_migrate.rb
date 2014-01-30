118ab64c3bacf4e457777e727da33024f77ce19b
Daniel Esplin <daniel@custombit.com>
2014-01-29 21:32:27 -0700

    Fix creating secrets file.

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -4,7 +4,7 @@ file "#{resource.shared_path}/config/secrets.yml" do
   owner resource.user
   group resource.group
   mode '0660'
-  content resource.params[:deploy_data]['secrets'].map { |k, v| "#{k.upcase}: '#{v}'" }.join("\n")
+  content resource.params[:deploy_data]['secrets'].map { |k, v| "production:\n  #{k.upcase}: '#{v}'" }.join("\n")
   action :create
   only_if { ::File.exists?(resource.shared_path) }
 end
