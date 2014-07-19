0f4102dcffd7f9f2d07c0cee3be7024b0f8b229e
Daniel Esplin <daniel@custombit.com>
2014-07-19 14:56:26 -0600

    Remove delayed jobs.

--- deploy/before_restart.rb
+++
@@ -1,7 +0,0 @@
-resource = new_resource
-execute 'delayed_job restart' do
-  cwd release_path
-  command 'bin/delayed_job restart'
-  environment 'RAILS_ENV' => resource.environment['RAILS_ENV']
-  only_if { ::File.exists?(release_path)}
-end
