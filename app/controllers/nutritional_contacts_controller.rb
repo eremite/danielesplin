58522ef3d2096349363d5dc647ccbf37c8657907
Daniel Esplin <daniel@custombit.com>
2014-10-31 14:59:39 -0600

    Uncomment delivering emails.

--- app/controllers/nutritional_contacts_controller.rb
+++ app/controllers/nutritional_contacts_controller.rb
@@ -6,7 +6,7 @@ class NutritionalContactsController < ApplicationController
     if params[:body].present?
       user = User.where(role: 'father')
       user.entries.create(body: "New contact: #{params.inspect}")
-      # Notifier.nutritional_contact_notification(params[:name], params[:email], params[:body]).deliver
+      Notifier.nutritional_contact_notification(params[:name], params[:email], params[:body]).deliver
     end
     redirect_to [:public_nutritional_posts], notice: 'Contact sent. Thanks!'
   end
