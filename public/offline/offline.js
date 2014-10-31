83045328a73c0b955f8320531d05715575755e10
Daniel Esplin <daniel@custombit.com>
2014-10-31 14:21:40 -0600

    Rework and fix offline support.

--- public/offline/offline.js
+++ public/offline/offline.js
@@ -1,71 +1,14 @@
 (function() {
-  var renderEntries;
 
-  jQuery(function() {
-    $('.datetimepicker').datetimepicker({
-      dateFormat: 'm/d/yy',
-      timeFormat: 'h:mmtt',
-      ampm: true,
-      altFormat: 'yy-mm-dd',
-      altTimeFormat: 'hh:mm:ss TT z',
-      altField: '.datetimepicker-hidden',
-      altFieldTimeOnly: false,
-      useLocalTimezone: true,
-      showTimezone: false,
-      showSecond: false
-    });
-    $('.datepicker').datepicker();
-    $('.autofocus:first').focus();
+  var resetForm = function() {
+    $('form#new_entry')[0].reset();
+    var today = new Date();
+    today.setMinutes(today.getMinutes() - today.getTimezoneOffset());
+    $('#entry_at').val(today.toJSON().slice(0, 10));
+    $('#entry_body').focus();
+  }
 
-    if (!localStorage['entries']) {
-      localStorage['entries'] = JSON.stringify([]);
-    } else {
-      renderEntries();
-    }
-
-    $('form#new_entry').on('submit', function(e) {
-      e.preventDefault();
-      if ($('#at').val().length === 0 || $('#entry_body').val().length === 0 || $('#api_key').val().length === 0) {
-        alert("Date or Content or API Key is empty!"); return;
-      }
-      var entries, entry;
-      entries = $.parseJSON(localStorage['entries']);
-      entry = {
-        title: '' + $('#api_key').val() + ' ' + $('#at').val(),
-        body: $('#entry_body').val(),
-        data: $(this).serialize()
-      };
-      entries.push(entry);
-      localStorage['entries'] = JSON.stringify(entries);
-      renderEntries();
-      this.reset();
-    });
-
-    return $('a#sync-with-server').on('click', function(e) {
-      var entries, entry, i, length;
-      entries = $.parseJSON(localStorage['entries']);
-      for (i = 0, length = entries.length; i < length; i++) {
-        entry = entries[i];
-        $.ajax({
-          type: 'POST',
-          url: '/api/entries',
-          data: entry.data,
-          success: function() {
-            console.log('Uploaded', entry);
-          },
-          error: function(data) {
-            alert(data.responseText);
-            $('ul.errors').append('<li>' + entry.title + ' - ' + entry.body + '</li>');
-          }
-        });
-      }
-      localStorage['entries'] = JSON.stringify([]);
-      renderEntries();
-      return e.preventDefault();
-    });
-  });
-
-  renderEntries = function() {
+  var renderEntries = function() {
     var entry, _i, _len, _ref, _results;
     $('ul.entries').empty();
     _ref = $.parseJSON(localStorage['entries']);
@@ -76,5 +19,59 @@
     }
     return _results;
   };
+
+
+  jQuery(function() {
+
+    resetForm();
+
+    if (!localStorage['entries']) {
+      localStorage['entries'] = JSON.stringify([]);
+    } else {
+      renderEntries();
+    }
+
+    $('form#new_entry').on('submit', function(e) {
+      e.preventDefault();
+      if ($('#entry_at').val().length === 0 || $('#entry_body').val().length === 0 || $('#api_key').val().length === 0) {
+        alert("Date or Content or API Key is empty!"); return;
+      }
+      var entries, entry;
+      entries = $.parseJSON(localStorage['entries']);
+      entry = {
+        title: '' + $('#api_key').val() + ' ' + $('#entry_at').val(),
+        body: $('#entry_body').val(),
+        data: $(this).serialize()
+      };
+      entries.push(entry);
+      localStorage['entries'] = JSON.stringify(entries);
+      renderEntries();
+      resetForm();
+    });
+
+    return $('a#sync-with-server').on('click', function(e) {
+      var entries, i, length;
+      entries = $.parseJSON(localStorage['entries']);
+      for (i = 0, length = entries.length; i < length; i++) {
+        (function (entry) {
+          $.ajax({
+            type: 'POST',
+            url: '/api/entries',
+            data: entry.data,
+            success: function() {
+              console.log('Uploaded', entry);
+            },
+            error: function(data) {
+              alert(data.responseText);
+              $('ul.errors').append('<li>' + entry.title + ' - ' + entry.body + '</li>');
+            }
+          });
+        })(entries[i]);
+      }
+      localStorage['entries'] = JSON.stringify([]);
+      renderEntries();
+      return e.preventDefault();
+    });
+  });
 
 }).call(this);
