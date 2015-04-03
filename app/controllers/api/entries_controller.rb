192eec9eab1eda5418f52095a5d4958cb79c53e2
Daniel Esplin <daniel@custombit.com>
2015-04-03 17:37:19 -0600

    API: Responders no longer come with Rails.

--- app/controllers/api/entries_controller.rb
+++ app/controllers/api/entries_controller.rb
@@ -1,11 +1,9 @@
 class Api::EntriesController < ApiController
 
-  respond_to :json
-
   def create
     @entry = @user.entries.new(safe_params)
     if @entry.save
-      respond_with(@entry, status: :created)
+      render json: @entry, status: :created
     else
       render text: @entry.errors.full_messages.to_sentence, status: :not_acceptable
     end
