9565775be7dd872549060b7ce7d06863403b94a5
Daniel Esplin <daniel@custombit.com>
2017-04-18 12:29:01 -0600

    Set Cache-Control on assets uploaded to S3.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,4 +6,5 @@ CarrierWave.configure do |config|
   }
   config.fog_directory = Rails.application.secrets.fog_bucket.to_s
   config.cache_dir = 'system/tmp'
+  config.fog_attributes = { 'Cache-Control' => "public, max-age=#{1.year.to_i}" }
 end
