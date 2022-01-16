0035eb8d185810bf5de027c61f80720261762f23
Daniel Esplin <daniel@danielesplin.org>
2022-01-16 14:52:43 -0700

    Don't crash on images that don't have exifr data.
    Like screenshots.

--- app/classes/photo_batch.rb
+++ app/classes/photo_batch.rb
@@ -9,10 +9,20 @@ class PhotoBatch
     self.errors = []
     images.each do |image|
       photo = user.photos.new(image: image)
-      photo.at = Time.zone.parse(EXIFR::JPEG.new(image.open).date_time.to_s.first(20))
+      photo.at = extract_time(image)
       self.errors << photo.errors.full_messages unless photo.save
     end
     true
+  end
+
+  private
+
+  def extract_time(image)
+    begin
+      Time.zone.parse(EXIFR::JPEG.new(image.open).date_time.to_s.first(20))
+    rescue EXIFR::MalformedJPEG
+      Time.current
+    end
   end
 
 end
