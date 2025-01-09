f69621e6e98f64c980d8348392230d6a3308f353
Daniel Esplin <daniel@danielesplin.org>
2025-01-09 15:53:18 -0700

    Attempt to extract photo time from filename.
    Signed-off-by: Daniel Esplin <daniel@danielesplin.org>

--- app/classes/photo_batch.rb
+++ app/classes/photo_batch.rb
@@ -10,7 +10,7 @@ class PhotoBatch
     images.each do |image|
       next if image.blank?
       photo = user.photos.new(image: image)
-      photo.image.open { |tempfile| photo.at = extract_time(tempfile) }
+      photo.image.open { |tempfile| photo.at = extract_time(tempfile, photo.image.blob.filename.to_s) }
       self.errors << photo.errors.full_messages unless photo.save
     end
     true
@@ -18,12 +18,19 @@ class PhotoBatch
 
   private
 
-  def extract_time(file)
-    begin
-      Time.zone.parse(EXIFR::JPEG.new(file).date_time.to_s.first(20))
-    rescue EXIFR::MalformedJPEG
-      Time.current
-    end
+  def extract_time(file, filename)
+    exif_time(file) || filename_time(filename) || Time.current
+  end
+
+  def exif_time(file)
+    Time.zone.parse(EXIFR::JPEG.new(file).date_time.to_s.first(20))
+  rescue EXIFR::MalformedJPEG
+    nil
+  end
+
+  def filename_time(filename)
+    match = filename.match(/.*(?<date>\d{8})_(?<time>\d{6}).*/)
+    Time.zone.strptime("#{match[:date]} #{match[:time]}", '%Y%m%d %H%M%S') rescue nil
   end
 
 end
